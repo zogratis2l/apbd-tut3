3e84020412947c6f2c3eb04c5af6cfe37d852a9a
Roman Tuziuk <s32809@pjwstk.edu.pl>
2026-03-17 11:41:27 +0100

    Partly implementation of Rental

---
+++ apbd-tut3/apbd-tut3/Rental.cs
@@ -0,0 +1,32 @@
+﻿using apbd_tut3.Users;
+
+namespace apbd_tut3;
+
+public class Rental
+{
+    private static int nextId = 1;
+    
+    public User user { get; set; }
+    
+    public Equipment equipment { get; set; }
+    
+    public int Id { get; set; }
+    
+    DateTime RantalDate { get; set; }
+    
+    DateTime RentalTime { get; set; }
+    
+    DateTime ReturnDate { get; set; }
+
+
+    public Rental(User who, Equipment what)
+    {
+        Id = nextId++;
+        user = who;
+        equipment = what;
+        RantalDate = DateTime.Now;
+        
+    }
+    
+    
+}
